282f152c8447d6694707e7e5fd1c720a8945557a
SmokingA1 <denisshokolad1221@gmail.com>
2025-05-17 21:56:00 +0300

    Add deck services

--- backend/app/services/deck.py
+++ backend/app/services/deck.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, List
 from uuid import UUID
 
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -8,25 +8,71 @@ from app.core.config import settings
 from app.core.database import SessionDep
 from app.models import Deck
 from app.schemas import (
-    DeckBase,
-    DeckRead
+    DeckCreate,
+    DeckUpdate
 )
 
-
-async def get_deck_by_id(db: AsyncSession, deck_id: UUID) -> Any:   
+async def get_deck_by_id(db: AsyncSession, deck_id: UUID) -> Deck | None:   
     db_deck = await db.get(Deck, deck_id)
     return db_deck
 
 
-async def get_deck_by_user_id(db: AsyncSession, owner_id: UUID) -> Any:
+async def get_deck_by_user_id(db: AsyncSession, owner_id: UUID) -> Deck | None:
     query = await db.execute(select(Deck).where(Deck.owner_id == owner_id))
     db_deck = query.scalars().first()
     return db_deck
 
 
 async def get_decks(
-    db: AsyncSession
-) -> Any:
-    query = await db.execute(select(Deck))
+    db: AsyncSession,
+    offset: int,
+    limit: int,
+) -> List[Deck] | None:
+    query = await db.execute(select(Deck).offset(offset).limit(limit))
     db_decks = query.scalars().all()
     return db_decks
+
+
+async def get_decks_by_user_id(
+    db: AsyncSession, owner_id: UUID
+) -> List[Deck] | None:
+    query = await db.execute(select(Deck).where(Deck.owner_id == owner_id))
+    db_decks = query.scalars().all()
+    return db_decks
+
+
+async def create_deck(db: AsyncSession, deck_create: DeckCreate) -> Deck | None:
+    new_deck = Deck(
+        name = deck_create.name,
+        owner_id = deck_create.ownder_id
+    )
+
+    db.add(new_deck)
+    await db.commit()
+    await db.refresh(new_deck)
+    return new_deck
+
+
+async def update_deck_name(db: AsyncSession, deck_id: UUID, deck_update: DeckUpdate) -> Deck | None:
+    db_deck = await get_deck_by_id(db, deck_id)
+    
+    if not db_deck:
+        return None
+    
+    db_deck.name = deck_update.name
+    await db.commit()
+    await db.refresh(db_deck)
+
+    return db_deck
+
+
+async def delete_deck_by_id(db: AsyncSession, deck_id: UUID) -> Deck | None:
+    db_deck = await get_deck_by_id(db, deck_id)
+    
+    if not db_deck:
+        return None
+    
+    await db.delete(db_deck)
+    await db.commit()
+
+    return db_deck
